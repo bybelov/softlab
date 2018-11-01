4364badb472c9ab350727c14b3a9df59c116b07d
Igor <bybelov@yandex.ru>
2018-11-01 23:25:21 +0300

    form +2h

--- src/js/app.js
+++ src/js/app.js
@@ -7,13 +7,17 @@ $(document).ready(function () {
   let inputTel = document.querySelector('.tel');
 
   if (inputTel != null) {
+
     const phoneMask = ['+', '7', ' ', '(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, '-', /\d/, /\d/];
-    maskInput({
-      inputElement: inputTel,
-      mask: phoneMask,
-      keepCharPositions: true,
-      showMask: true
+    inputTel.addEventListener('click', function(){
+      maskInput({
+        inputElement: inputTel,
+        mask: phoneMask,
+        keepCharPositions: true,
+        showMask: true
+      });
     });
+
 
   }
 
